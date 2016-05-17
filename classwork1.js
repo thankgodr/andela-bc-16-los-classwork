41263ede8c9af543bdf4298d83f8a394ba5a47ca
ThankGod Richard <madueketf@gmail.com>
2016-05-17 17:13:53 +0100

    Task 2 done

--- classwork1.js
+++ classwork1.js
@@ -1,38 +1,40 @@
 function NotesApplication(author){
 	this.author = author;
-	this.notelist = [];
+	this.notelist = [];	
 
-	 this.create = function(note_content,note_id){
+}
+
+NotesApplication.prototype.create = function(note_content,note_id){
          notelist.push([note_id,note_content]);
-	}
+}
 
-	this.listNotes() = function(){
+NotesApplication.prototype.listNotes = function(){
 		return notelist;
-		//------the block code bellow show how it can me manipulated on the DOM-----\\
+		//------this block  bellow show how it can me manipulated on the DOM-----\\
 		/*for(var i = 0; i < notelist.length; i++){
 			console.log("Note ID: "notelist[i][0]);
 			console.log("Note ID: "notelist[i][1]);
 			console.log("By Author " this.author);
 		*/
-	}
+}
 
-	this.get = function(note_id){
+NotesApplication.prototype.get = function(note_id){
 		obj = []
-		for(var i =0; i <notelist.length; i++){
-			if(if note_id === notelist[i][0]){
-                obj.push(notelist[i][0]) ;
-                obj.push(notelist[i][1]) ; 
-			}
-		}
-		if(obj.length > 0){
-			return obj
-		}
-		else{
-			return "No ID Found"
+	for(var i =0; i <notelist.length; i++){
+		if(if note_id === notelist[i][0]){
+            obj.push(notelist[i][0]) ;
+            obj.push(notelist[i][1]) ; 
 		}
 	}
+	if(obj.length > 0){
+		return obj
+	}
+	else{
+		return "No ID Found"
+	}
+}
 
-	this.search = function(search_text){
+NotesApplication.prototype.search = function(search_text){
 		obj = []
 		for(var i =0; i <notelist.length; i++){
 			if(if note_id === notelist[i][1]){
@@ -47,7 +49,4 @@ function NotesApplication(author){
 			return "No text Found"
 		}
 	}
-	
-
-}
 
