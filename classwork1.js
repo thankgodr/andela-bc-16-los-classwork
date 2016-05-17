044afb5f5acd619fd064563a74ecf045d88632aa
ThankGod Richard <madueketf@gmail.com>
2016-05-17 17:03:42 +0100

    Task 2 done

--- classwork1.js
+++ classwork1.js
@@ -1,21 +1,51 @@
 function NotesApplication(author){
 	this.author = author;
-	var notelist = [];
+	this.notelist = [];
 
 	 this.create = function(note_content,note_id){
-         notelist.push([note_id,note_content])
+         notelist.push([note_id,note_content]);
 	}
 
 	this.listNotes() = function(){
-		for(var i = 0; i < notelist.length; i++){
+		return notelist;
+		//------the block code bellow show how it can me manipulated on the DOM-----\\
+		/*for(var i = 0; i < notelist.length; i++){
 			console.log("Note ID: "notelist[i][0]);
 			console.log("Note ID: "notelist[i][1]);
 			console.log("By Author " this.author);
-		}
+		*/
 	}
 
 	this.get = function(note_id){
-		
+		obj = []
+		for(var i =0; i <notelist.length; i++){
+			if(if note_id === notelist[i][0]){
+                obj.push(notelist[i][0]) ;
+                obj.push(notelist[i][1]) ; 
+			}
+		}
+		if(obj.length > 0){
+			return obj
+		}
+		else{
+			return "No ID Found"
+		}
+	}
+
+	this.search = function(search_text){
+		obj = []
+		for(var i =0; i <notelist.length; i++){
+			if(if note_id === notelist[i][1]){
+                obj.push(notelist[i][0]) ;
+                obj.push(notelist[i][1]) ; 
+			}
+		}
+		if(obj.length > 0){
+			return obj
+		}
+		else{
+			return "No text Found"
+		}
 	}
 	
 
