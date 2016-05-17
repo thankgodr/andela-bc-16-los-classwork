7f8e67fde52709d41406e9434e77cf4a11b872cd
ThankGod Richard <madueketf@gmail.com>
2016-05-17 17:46:38 +0100

    Task 2 corrected

--- classwork1.js
+++ classwork1.js
@@ -4,26 +4,22 @@ function NotesApplication(author){
 
 }
 
-NotesApplication.prototype.create = function(note_content,note_id){
-         notelist.push([note_id,note_content]);
+NotesApplication.prototype.create = function(note_content){
+         notelist.push(note_content);
 }
 
 NotesApplication.prototype.listNotes = function(){
-		return notelist;
-		//------this block  bellow show how it can me manipulated on the DOM-----\\
-		/*for(var i = 0; i < notelist.length; i++){
-			console.log("Note ID: "notelist[i][0]);
-			console.log("Note ID: "notelist[i][1]);
+		for(var i = 0; i < notelist.length; i++){
+			console.log("Note ID: " + i);
+			console.log("Note ID: "notelist[i]);
 			console.log("By Author " this.author);
-		*/
 }
 
 NotesApplication.prototype.get = function(note_id){
 		obj = []
 	for(var i =0; i <notelist.length; i++){
-		if(if note_id === notelist[i][0]){
-            obj.push(notelist[i][0]) ;
-            obj.push(notelist[i][1]) ; 
+		if(if note_id === i){
+            obj.push(notelist[i]) ;
 		}
 	}
 	if(obj.length > 0){
@@ -37,7 +33,7 @@ NotesApplication.prototype.get = function(note_id){
 NotesApplication.prototype.search = function(search_text){
 		obj = []
 		for(var i =0; i <notelist.length; i++){
-			if(if note_id === notelist[i][1]){
+			if( !notelist[i].search(search_text) == -1){
                 obj.push(notelist[i][0]) ;
                 obj.push(notelist[i][1]) ; 
 			}
