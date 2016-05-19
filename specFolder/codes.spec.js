01d1872a55e8cdb2386ff4e33295022ea24167ca
ThankGod Richard <madueketf@gmail.com>
2016-05-19 08:15:12 +0100

    Directory change

---
+++ specFolder/codes.spec.js
@@ -0,0 +1,152 @@
+//Main Class
+function NotesApplication(author){
+	this.author = author;
+	this.notelist = [];	
+
+}
+
+
+// function that takes the note content as the parameter and adds it to the notes list of the objec.
+NotesApplication.prototype.create = function(note_content){
+       this.notelist.push(note_content);
+       return "Content Created"
+};
+
+
+
+//This function lists out each of the notes in the notes list
+NotesApplication.prototype.listNotes = function(){
+	obj = []; // local variable
+	for(var i =0; i < this.notelist.length; i++){
+		if(this.notelist.length === 0){
+			return "Content is Empty";  // not working
+		}
+		else{
+			obj.push({1: "Note ID: " + i, 
+				      2: this.notelist[i],
+				      3: "By Author " + this.author + "\n"
+				});
+		}
+	}
+	//for testing purposes 
+		for(var j = 0; j <obj.length; j++){
+		for(var key in obj[j]){
+			console.log(obj[j][key]);
+		}
+	}
+};
+
+/*----This function takes a note_id which refers to the index of the note in the notes list and returns the content of that note as a string-----*/
+NotesApplication.prototype.get = function(note_id){
+	if(Number.isInteger(note_id)){
+			obj = [];
+	
+		for(var i =0; i < this.notelist.length; i++){
+			if(note_id === i){
+	            obj.push(this.notelist[i]) ;
+			}
+		}
+		if(obj.length > 0){
+			return obj.join(" ");
+		}
+		else{
+			return "No ID Found";
+		}
+    }
+    else{
+		return "Your Input is not a number";
+	}
+
+};
+
+
+/*This function take a search string, search_text and returns all the notes with that text within it
+*/
+NotesApplication.prototype.search = function(search_text){
+	var obj = [];
+	for(var i =0; i < this.notelist.length; i++){
+		if( this.notelist[i].toLowerCase().search(search_text.toLowerCase()) == -1){
+			return "Text does not exist";
+		}
+		else{
+			obj.push({1: "Note ID: " + i, 
+				      2: this.notelist[i],
+				      3: "By Author " + this.author + "\n"
+				    });
+		}
+	}
+	//for testing purposes only
+	for(var j = 0; j <obj.length; j++){
+		for(var key in obj[j]){
+			console.log(obj[j][key]);
+		}
+	}	
+};
+	
+//This function deletes the note at the index note_id of the notes list
+NotesApplication.prototype.delete = function(note_id){
+	if(note_id > this.notelist.length || note_id < 0){
+		return "Id does not Exist";
+	}
+	else{
+		this.notelist.splice(note_id,1);
+		return "Content Deleted";	
+	}
+	
+};
+
+//This function edit the note at the index note_id of the notes list
+NotesApplication.prototype.edit = function(note_id,new_content){
+	if(note_id > this.notelist.length || note_id < 0){
+		return "ID does not Exist";
+	}
+	else{
+		this.notelist[note_id] = new_content;
+		return "Content Updated";
+	}
+	
+};
+
+
+
+describe("NotesApplication  function", function() {
+    var noteme = new NotesApplication("Peter") ;    
+    it("NotesApplication should have authour as parenthesis", function() {
+        expect(noteme.author).toEqual("Peter");
+    });
+ 
+    it("Check for NotesApplication has property Create", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("create")).toBe(true);
+    });
+ 
+    it("Check for NotesApplication has property listnote", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("listNotes")).toBe(true);
+    });
+
+    it("Check for NotesApplication has property Get", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("get")).toBe(true);
+    });
+ 
+     it("Check for NotesApplication has property search", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("search")).toBe(true);
+    });
+ 
+     it("Check for NotesApplication has property delete", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("delete")).toBe(true);
+    });
+
+    it("Check for NotesApplication has property edit", function() {
+        expect(NotesApplication.prototype.hasOwnProperty("edit")).toBe(true);
+    });
+
+    it("Check for NotesApplication Create method to return appropraite ", function() {
+        expect(noteme.create("hi")).toBe("Content Created");
+    });
+
+    it("Check for NotesApplication get method to return appropraite ", function() {
+        expect(noteme.get("a")).toBe("Your Input is not a number");
+    });
+    it("Check for NotesApplication get method to return appropraite ", function() {
+        expect(noteme.edit("4")).toBe("ID does not Exist");
+    });
+});
